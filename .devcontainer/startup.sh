a3444b89517874aa2034483e861fab0bb33cd9d6
mkrangrej08-web <m.k.rangrej08@gmail.com>
2026-09-01 15:33:04 +0530

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
-
-curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-sleep 4
+sleep 3
 sudo tailscale up --authkey="tskey-auth-kp4D8uZ79V11CNTRL-cwVtEnFQAEcnJsRVMK4YDcDrrygXERVa" --hostname="codespace-desktop"
 
-bash /workspaces/THE-LINUX/start-desktop-vnc.sh &
+if [ -f "/workspaces/THE-LINUX/start-desktop-vnc.sh" ]; then
+    bash /workspaces/THE-LINUX/start-desktop-vnc.sh &
+fi
